ab46a0ea7cd1ffdf93021ec802e47f1d3b1fe537
Kristine Erickson <ericksonk09@gmail.com>
2015-10-25 13:16:56 -0400

    linked to live api

--- src/js/main.js
+++ src/js/main.js
@@ -44,8 +44,8 @@ $('.q-vote > a[href] > i').on('click', function(){
 
 
   .run(function($http, $rootScope){
-    // $http.get('https://jakd.herokuapp.com/questions')
-    $http.get('/apis/questions.json')
+    $http.get('https://jakd.herokuapp.com/questions.json')
+    // $http.get('/apis/questions.json')
       .then(function(response){
         $rootScope.questions = response.data;
       });
